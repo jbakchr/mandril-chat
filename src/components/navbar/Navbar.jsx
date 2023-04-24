0a249b0024bcab72b418eae213f4aa7af9a97c6d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-24 20:11:49 +0200

    Added navbar styling and fake chat messages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,16 @@
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "blue",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        height: "50px",
+      }}
+    >
+      <h1>Navbar</h1>
+    </div>
+  );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,20 @@ export const HomePage = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <Navbar />
-      <p style={{ flexGrow: 1 }}>Chat messages list</p>
+      <div style={{ flexGrow: 1 }}>
+        <p style={{ border: "1px solid black", height: "400px" }}>
+          Chat message
+        </p>
+        <p style={{ border: "1px solid black", height: "400px" }}>
+          Chat message
+        </p>
+        <p style={{ border: "1px solid black", height: "400px" }}>
+          Chat message
+        </p>
+        <p style={{ border: "1px solid black", height: "400px" }}>
+          Chat message
+        </p>
+      </div>
       <p>Footer</p>
     </div>
   );
