a67d180f4115145cf5efbb59f6d2ea3750b261d7
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-24 19:59:31 +0200

    Added initial layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,9 @@
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <p>Navbar</p>
+      <p style={{ flexGrow: 1 }}>Chat messages list</p>
+      <p>Footer</p>
+    </div>
+  );
 };
