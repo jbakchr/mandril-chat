d849968960786992d9deb8e498dba6eb5f67fd3e
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-24 20:23:13 +0200

    Added initial footer styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,16 @@
 export const Footer = () => {
-  return <div style={{ backgroundColor: "#0098c5" }}>Footer</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "#0098c5",
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: "50px",
+      }}
+    >
+      Footer
+    </div>
+  );
 };
