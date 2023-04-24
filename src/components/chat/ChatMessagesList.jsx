0877d23fbc31cff25f68904b5b301ab050af95e9
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-24 20:26:31 +0200

    Added margin top and padding bottom to chat messages list

--- src/components/chat/ChatMessagesList.jsx
+++ src/components/chat/ChatMessagesList.jsx
@@ -1,6 +1,6 @@
 export const ChatMessagesList = () => {
   return (
-    <div style={{ flexGrow: 1 }}>
+    <div style={{ flexGrow: 1, marginTop: "50px", paddingBottom: "50px" }}>
       <p style={{ border: "1px solid black", height: "200px" }}>Chat message</p>
       <p style={{ border: "1px solid black", height: "200px" }}>Chat message</p>
       <p style={{ border: "1px solid black", height: "200px" }}>Chat message</p>
