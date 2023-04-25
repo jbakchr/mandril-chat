83cf9a560c515207ab7fcd54b93f38bce2d23215
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 04:26:25 +0200

    Deleted avatar content component

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,6 +1,5 @@
 import { MainLayout } from "../layout/MainLayout";
 import { Navbar } from "../components/navbar/Navbar";
-import { AvatarContent } from "../components/avatar/AvatarContent";
 import { Footer } from "../components/footer/Footer";
 import { AvatarHeader } from "../components/avatar/AvatarHeader";
 import { MainContent } from "../layout/MainContent";
