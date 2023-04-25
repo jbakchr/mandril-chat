e4894902ae41b074c0d9c8cdaeb8f71f37e4199d
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 09:54:02 +0200

    deleted fake chat message data

--- src/components/chat/ChatMessagesList.jsx
+++ src/components/chat/ChatMessagesList.jsx
@@ -1,10 +1,7 @@
 export const ChatMessagesList = () => {
   return (
-    <>
-      <p style={{ border: "1px solid black", height: "200px" }}>Chat message</p>
-      <p style={{ border: "1px solid black", height: "200px" }}>Chat message</p>
-      <p style={{ border: "1px solid black", height: "200px" }}>Chat message</p>
-      <p style={{ border: "1px solid black", height: "200px" }}>Chat message</p>
-    </>
+    <ul>
+      <li>Numse</li>
+    </ul>
   );
 };
