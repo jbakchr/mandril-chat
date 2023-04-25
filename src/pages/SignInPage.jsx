26e8dbbe125f983b0ade1ef4eee02b2f3ea72882
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 02:43:53 +0200

    Added initial sign in page structure

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Navbar } from "../components/navbar/Navbar";
+import { Footer } from "../components/footer/Footer";
 
 export const SignInPage = () => {
-  return <div>SignInPage</div>;
+  return (
+    <div>
+      <Navbar />
+      <div style={{ flexGrow: 1 }}>Blah</div>
+      <Footer />
+    </div>
+  );
 };
