846860b9e680e9f829652d2a5b50bbf5ae0d1e9a
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 09:21:54 +0200

    Added user from context to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,24 @@
+import { useContext } from "react";
+
+import { GlobalContext } from "../context/GlobalContext";
 import { MainLayout } from "../layout/MainLayout";
 import { Navbar } from "../components/navbar/Navbar";
+import { MainContent } from "../layout/MainContent";
 import { ChatMessagesList } from "../components/chat/ChatMessagesList";
 import { Footer } from "../components/footer/Footer";
 import { SignInButton } from "../components/buttons/SignInButton";
-import { MainContent } from "../layout/MainContent";
 
 export const HomePage = () => {
+  const { user } = useContext(GlobalContext);
+  console.log("user:", user);
+
   return (
     <MainLayout>
       <Navbar />
       <MainContent>
         <ChatMessagesList />
       </MainContent>
-      <Footer>
-        <SignInButton />
-      </Footer>
+      <Footer>{!user ? <SignInButton /> : <p>Show input</p>}</Footer>
     </MainLayout>
   );
 };
