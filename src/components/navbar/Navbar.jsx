c800ee52079ac13cb9e9b20304eec6ad91f616ca
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 03:07:03 +0200

    Added navbar styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,28 @@
+const navbarStyle = {
+  backgroundColor: "#0098c5",
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  height: "50px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+};
+
 export const Navbar = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "#0098c5",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        height: "50px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
+    <div style={navbarStyle}>
       <p>logo</p>
-      <h1>Navbar</h1>
+      <h1
+        style={{
+          fontFamily: "Lobster Two",
+          color: "white",
+          textShadow: "2px 2px black",
+        }}
+      >
+        Mandril Chatten
+      </h1>
       <p>user</p>
     </div>
   );
