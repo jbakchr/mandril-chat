a32ea6c53a291330f6c9fa4da9ebb4b2f167599f
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 07:24:29 +0200

    Added images to images list

--- src/components/avatar/AvatarImageList.jsx
+++ src/components/avatar/AvatarImageList.jsx
@@ -1,3 +1,23 @@
+const images = [
+  { url: "bianca_landsvig.png", altText: "Bianca Landsvig" },
+  { url: "broder_salsa.png", altText: "Broder Salsa" },
+  { url: "david_hasselhof.png", altText: "David Hasselhof" },
+  { url: "kenneth.png", altText: "Kenneth" },
+  { url: "laughing_eagle.png", altText: "Laughing Eagle" },
+];
+
 export const AvatarImageList = () => {
-  return <>AvatarImageList</>;
+  return (
+    <div>
+      {images.map((img) => {
+        return (
+          <img
+            src={`/images/${img.url}`}
+            alt={img.altText}
+            style={{ width: "50px", borderRadius: "50%", margin: "3px" }}
+          />
+        );
+      })}
+    </div>
+  );
 };
