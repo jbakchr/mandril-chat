8741941e0a13a817f3f10659b7127d3e778ed089
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 10:04:36 +0200

    Added setup of firebase

---
+++ src/firebase/firebaseApp.jsx
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTCyazEJ9Y_msrlgZLM3tC4WI-MtdoiOg",
+  authDomain: "mandril-cfb1e.firebaseapp.com",
+  projectId: "mandril-cfb1e",
+  storageBucket: "mandril-cfb1e.appspot.com",
+  messagingSenderId: "1095411548633",
+  appId: "1:1095411548633:web:59ac4d3262d4e1423759c8",
+  measurementId: "G-XXSN6TGT48",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
