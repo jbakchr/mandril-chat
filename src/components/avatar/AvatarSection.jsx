d9723bc242975ba8252db960ca5e9b94b3834475
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 06:35:46 +0200

    Added border. width and flex to avatar section

--- src/components/avatar/AvatarSection.jsx
+++ src/components/avatar/AvatarSection.jsx
@@ -1,3 +1,16 @@
 export const AvatarSection = ({ children }) => {
-  return <div style={{ margin: "10px 0px" }}>{children}</div>;
+  return (
+    <div
+      style={{
+        margin: "10px 0px",
+        border: "1px solid black",
+        width: "300px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
