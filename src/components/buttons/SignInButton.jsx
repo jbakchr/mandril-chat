14fa28da80650e2b37bb8963317efe7657655997
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 02:37:56 +0200

    Added link to sign in page

--- src/components/buttons/SignInButton.jsx
+++ src/components/buttons/SignInButton.jsx
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom";
+
 export const SignInButton = () => {
   return (
-    <button
-      style={{
-        height: "40px",
-        padding: "0px 10px",
-        backgroundColor: "#ffd32a",
-        borderRadius: "50px",
-        fontFamily: "Lobster Two",
-        fontSize: "17px",
-        letterSpacing: "2px",
-        color: "#01a75b",
-      }}
-    >
-      Deltag i chatten
-    </button>
+    <Link to="/signin">
+      <button
+        style={{
+          height: "40px",
+          padding: "0px 10px",
+          backgroundColor: "#ffd32a",
+          borderRadius: "50px",
+          fontFamily: "Lobster Two",
+          fontSize: "17px",
+          letterSpacing: "2px",
+          color: "#01a75b",
+        }}
+      >
+        Deltag i chatten
+      </button>
+    </Link>
   );
 };
