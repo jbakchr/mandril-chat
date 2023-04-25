d223b9f65773a280e9f02c64c79931d3f8cf9b64
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 04:14:47 +0200

    Changed avatar content styling

--- src/components/avatar/AvatarContent.jsx
+++ src/components/avatar/AvatarContent.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const AvatarContent = ({ children }) => {
-  return <div style={{ flexGrow: 1 }}>{children}</div>;
+  return (
+    <div
+      style={{
+        flexGrow: 1,
+        marginTop: "50px",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
