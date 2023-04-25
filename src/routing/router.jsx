1840d891c4efc085e61686b2e40b213c231cd0bc
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 02:35:45 +0200

    Added initial sign in page

--- src/routing/router.jsx
+++ src/routing/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
+import { SignInPage } from "../pages/SignInPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/signin",
+    element: <SignInPage />,
   },
 ]);
 
