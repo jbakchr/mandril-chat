8138f95656ee70b381711546b7eb5194d9ba24a8
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 04:15:33 +0200

    Added avatar header as child component of avatar content

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -2,12 +2,15 @@ import { MainLayout } from "../layout/MainLayout";
 import { Navbar } from "../components/navbar/Navbar";
 import { AvatarContent } from "../components/avatar/AvatarContent";
 import { Footer } from "../components/footer/Footer";
+import { AvatarHeader } from "../components/avatar/AvatarHeader";
 
 export const SignInPage = () => {
   return (
     <MainLayout>
       <Navbar />
-      <AvatarContent />
+      <AvatarContent>
+        <AvatarHeader />
+      </AvatarContent>
       <Footer />
     </MainLayout>
   );
