c88a2894ac84af9c7340480a356a6c43a9d1806f
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 02:54:39 +0200

    Added avatar content component

---
+++ src/components/avatar/AvatarContent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AvatarContent = ({ children }) => {
+  return <div style={{ flexGrow: 1 }}>{children}</div>;
+};
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,12 +1,13 @@
 import { MainLayout } from "../layout/MainLayout";
 import { Navbar } from "../components/navbar/Navbar";
+import { AvatarContent } from "../components/avatar/AvatarContent";
 import { Footer } from "../components/footer/Footer";
 
 export const SignInPage = () => {
   return (
     <MainLayout>
       <Navbar />
-      <div style={{ flexGrow: 1 }}>Blah</div>
+      <AvatarContent />
       <Footer />
     </MainLayout>
   );
