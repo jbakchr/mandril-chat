75c5932de82031574703c6f3d2239337aef73761
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 04:41:56 +0200

    Added avatar header styling

--- src/components/avatar/AvatarHeader.jsx
+++ src/components/avatar/AvatarHeader.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const AvatarHeader = () => {
-  return <h1>Vælg din avatar</h1>;
+  return (
+    <h1
+      style={{ fontFamily: "Lobster Two", color: "grey", letterSpacing: "2px" }}
+    >
+      Vælg din avatar
+    </h1>
+  );
 };
