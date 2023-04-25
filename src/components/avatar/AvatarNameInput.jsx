996fac32a6f6ebfd07674ebaef5ce8c2431880a0
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-04-25 07:28:17 +0200

    Added avatar name input styling

--- src/components/avatar/AvatarNameInput.jsx
+++ src/components/avatar/AvatarNameInput.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const AvatarNameInput = () => {
-  return <input type="text" style={{ width: "100%" }} />;
+  return (
+    <input
+      type="text"
+      style={{
+        width: "100%",
+        height: "30px",
+        outline: "none",
+      }}
+      placeholder="Skriv avatar navn her.."
+    />
+  );
 };
